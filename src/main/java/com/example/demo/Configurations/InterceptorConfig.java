eea1df7b3bb1cf1d54e039f4a50590d1d1bf9218
Manish R <manishraja2505@gmail.com>
2026-09-08 14:06:55 +0530

    feat: dockerized the spring app and db

--- src/main/java/com/example/demo/Configurations/InterceptorConfig.java
+++ src/main/java/com/example/demo/Configurations/InterceptorConfig.java
@@ -18,6 +18,6 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(interceptor).addPathPatterns("/**")
                 .excludePathPatterns("/login/**","/swagger-ui/**", "/v3/api" +
-                        "-docs/**", "/error");
+                        "-docs/**", "/error", "/actuator/**");
     }
 }
